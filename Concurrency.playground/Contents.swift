494f9eed0ee5a0a0d8f42905dd7df2971e328823
imayanknegi29 <imayanknegi29@gmail.com>
2025-01-05 19:41:22 +0530

    Introduction of Concurrency

--- Concurrency.playground/Contents.swift
+++ Concurrency.playground/Contents.swift
@@ -148,12 +148,11 @@ struct CountdownIterator: IteratorProtocol {
 
 let countdown = Countdown(start: 5)
 for number in countdown {
-    print(number)
+//    print(number)
 }
 
 /*
-/*
- ## Asynchronous Sequence
+ ## Asynchronous Sequence(work with a sequence of values that are delivered over time.)
 
  - listPhotos(inGallery:) returns whole array once the async execution returns.
  - If we want to wait only for one element of the collection at a time, we can do this using asynchronous sequence.
@@ -161,13 +160,67 @@ for number in countdown {
  - For this we need our type to conform to AsyncSequence
  */
 
+// build-in AsyncSequence(AsyncStream)
+func fetchNumber() -> AsyncStream<Int> {
+    AsyncStream { continuation in
+        Task {
+            for index in 1...5 {
+                continuation.yield(index) // send the value asynchronously.
+                try await Task.sleep(nanoseconds: 1_000_000_000) // 1 second delay
+            }
+            continuation.finish() // indicate the stream is done.
+        }
+    }
+}
+
 /*
-let handle = FileHandle.standardInput
-for try await line in handle.bytes.lines {
-    print(line)
+Task {
+    for await number in fetchNumber() {
+        print("Received : \(number)")
+    }
+    print("stream finished!")
 }
 */
 
+// Custom sequence
+struct Countdown2: AsyncSequence {
+    
+    typealias Element = Int
+
+    let start: Int
+
+    func makeAsyncIterator() -> Countdown2Iterator {
+        Countdown2Iterator(self)
+    }
+
+    struct Countdown2Iterator: AsyncIteratorProtocol {
+
+        var countdown: Countdown2
+        var current: Int
+
+        init(_ countdown: Countdown2) {
+            self.countdown = countdown
+            self.current = countdown.start
+        }
+
+        mutating func next() async -> Int? {
+            while(current != 0) {
+                defer { current -= 1 }
+                try? await Task.sleep(nanoseconds: 500_000_000) // 0.5 seconds
+                return current
+            }
+            return nil
+        }
+    }
+}
+
+Task {
+    let countdown2 = Countdown2(start: 5)
+    for await number in countdown2 {
+        print("Received async number seq: \(number)")
+    }
+    print("countdown finished!")
+}
 
 /*
  ## Calling Asynchronous functions in parallel
@@ -177,14 +230,14 @@ for try await line in handle.bytes.lines {
  - To call the asynchronous func and let it run in parallel with other asynchronous tasks, write async infront of let when you define a constant and use await everytime when use that constant.
  */
 
-func downloadDcument() async -> String {
+func downloadDocument() async -> String {
     print("Start document downloading...")
     try? await Task.sleep(for: .seconds(1))
     print("Document downloaded")
     return "Document fetch completed."
 }
 
-// Serial execution
+/* Serial execution */
 
 //Task {
 //    let firstPhoto = await downloadPhoto(named: "John")
@@ -197,11 +250,11 @@ func downloadDcument() async -> String {
 //    }
 //}
 
-// Concurrent/Parallel execution
+/* Concurrent/Parallel execution */
 
 Task {
     async let photo = downloadPhoto(named: "John")
-    async let document = downloadDcument()
+    async let document = downloadDocument()
 
     let files = await [photo, document] // constant is used with await
     for file in files {
@@ -215,9 +268,15 @@ Task {
 
  - Task is the unit of work that can run asynchronously as part of our programme. All asynchronous code run as a part of some task.
  - async-let syntax implicitely creates a child task.
- - We can also create a task group and explicitely add child task into task group.
+ - We can also create a task group and explicitely add child task into task group dynamically.
  - Each task in a given task group have a same parent task, and each task can have child tasks.
  - Because of this explicit relationship between task and task group, this approach is called `structured concurrency`.
+
+ parent-child relationship has below advantages:
+ - In a parent task, you can't forgot to wait for the child tasks to complete.
+ - When setting a higher priority to a child task, the parent task's priority automatically escalated.
+ - When a parent task is cancelled, all of its child tasks also automatically cancelled.
+ - Task local values propagated to child tasks efficiently and automatically.
  */
 
 Task {
@@ -238,6 +297,7 @@ Task {
     }
 }
 
+
 /*
  ## Task Cancellation
 
@@ -246,12 +306,35 @@ Task {
  - Task.isCancelled let us do the cleanup work as a part of stopping the task.
  */
 
+Task {
+    let photos = await withTaskGroup(of: Optional<String>.self) { group in
+        let photoNames = await listPhotos(inGallery: "Summer Vacation")
+        for photoName in photoNames {
+            let added = group.addTaskUnlessCancelled { // add the task if it is not cancelled.
+                guard !Task.isCancelled else { return nil } // chech whether the task is cancelled or not, if yes return nil else start downloading photo.
+                return await downloadPhoto(named: photoName)
+            }
+            guard added else { break } // check whether the task is successfully added if it is cancelled break out of loop.
+        }
+
+        var result: [String] = []
+        for await photo in group {
+            if let photo { result.append(photo) }
+        }
+        return result
+    }
+}
+
 
 /*
  ## Unstructured Concurrency
 
  - Unlike tasks that are the part of task group, an unstructured task doesn't have a parent task.
+ - It give devs complete flexibility to manage unstructured tasks in whatever way we the program needs.
+ - Task.init(priority:operation:)
+ - Task.detached(priority:operation:)
  */
+
 
 /*
 ## Actors
@@ -263,9 +346,8 @@ Task {
  - Like classes, actors are also reference type.
  - Unlike classes, actors only let one task access their mutable state at a time. Which makes it safe to interact with the same instance of the actor with the multimple tasks.
 
- - While accessing the actor property, we need to mark it with await keyword to create a suspention point. This let the other task to wait for the first task to access the actor property.
- - If we try to access actor's property inside from the same actor, in this case we don't have to write await keyword before it.
-
+ - While accessing the actor property, we need to mark it with `await` keyword to create a suspention point. This let the other task to wait for the first task to access the actor property.
+ - If we try to access actor's property inside from the same actor, in this case we don't have to write `await` keyword before it.
  */
 
 actor TemperatureLogger {
@@ -287,22 +369,80 @@ extension TemperatureLogger {
     func update(with measurement: Int) {
         measurements.append(measurement)
         if measurement > max {
-            max = measurement // The TemperatureLogger is in a temporary inconsistent state. Prevent multiple task to interact with same instance simultaneously.
+            max = measurement // The TemperatureLogger is in a temporary inconsistent state. Actor prevent multiple task to interact with same instance simultaneously which prevents any race condition here.
         }
     }
 }
 
 /*
-
  - The code to access the max property needs to run as a part of an actor, which is an asynchronous operation an requires writing await.
  - Swift guarantees that only code running as a part of actor can access the actor's local state, that is called - actor isolation.
  */
 
 
 /*
-
  ## Sendable Types
 
- -
+ - `Concurrency Domain` - A concurrency domain is a region of exexution where code runs without data race.
+ - Different concurrency domains can run independently(separate tasks, actors, threads).
+ - Swift sendable protocol ensures that data passed between domains is safe to share.
+
+ Common Concurrency Domains in Swift:
+- Main Actor (@MainActor) – Manages code execution on the main thread.
+- Actors – Provide isolated mutable state per instance.
+- Tasks – Represent units of asynchronous work (Task { ... }).
+- Global Executors – Default Swift executor for running tasks concurrently.
  */
-*/
+
+// Unsafe example
+class Counter {
+    var value: Int = 0
+}
+
+let counter = Counter()
+
+Task {
+    counter.value += 1  // Task 1: Concurrency Domain 1. Risky shared mutable state.
+}
+
+Task {
+    counter.value += 1  // Task 2: Concurrency Domain 2 Risky shared mutable state.
+}
+
+// safe example using actor
+actor Counter2 {
+    private var value: Int = 0
+
+    func increment() {
+        value += 1
+    }
+
+    func decrement() {
+        value -= 1
+    }
+
+    func getValue() -> Int {
+        value
+    }
+}
+
+let counter2 = Counter2()
+await counter2.increment()
+print(await counter2.getValue())
+
+// Sendable endures that values can be safely transferred between concurrency domains.
+// Actors are inherently sendable since they are designed to isolate state.
+// `@unchecked Sendable` tells the compiler, "Trust me, this type is safe across concurrency boundaries."
+
+
+// Sendable example
+struct User: Sendable {
+    let id: Int
+    let name: String
+}
+
+let user = User(id: 1, name: "User")
+
+Task {
+    print(user.name)
+}
